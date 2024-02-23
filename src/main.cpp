e3e0f6f391e4387f770e08cddba73e1c2598cf47
ubicoders <138417199+ubicoders0@users.noreply.github.com>
2024-02-23 14:17:38 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/time_source.hpp"
-
+#include "rosgraph_msgs/msg/clock.hpp"
 #include "std_msgs/msg/header.hpp"
 
 
@@ -12,11 +12,11 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
 
   auto node = rclcpp::Node::make_shared("time_example_node");
-  auto time_publisher = node->create_publisher<std_msgs::msg::Header>("time", 10);
-  std_msgs::msg::Header msg;
+  auto time_publisher = node->create_publisher<rosgraph_msgs::msg::Clock>("clock", 10);
+  rosgraph_msgs::msg::Clock msg;
 
   rclcpp::WallRate loop_rate(1.0);
-  rclcpp::Duration duration(1, 0);
+  rclcpp::Duration duration(-500000000, 0);
 
   while (rclcpp::ok()) {
     static rclcpp::Time past = node->now();
@@ -29,7 +29,7 @@ int main(int argc, char * argv[])
       past = node->now();
     }
 
-    msg.stamp = now + duration;
+    msg.clock.sec = now.seconds() + duration.seconds();
     time_publisher->publish(msg);
 
     rclcpp::spin_some(node);
